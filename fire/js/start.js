7eb1ddd1974084c1d313c32bbb178cde6ecc6a7d
bztd <wntex@yandex.com>
2023-09-12 08:22:19 -0500

    0

---
+++ fire/js/start.js
@@ -0,0 +1,7 @@
+var f=function{};
+
+var contenedor=document.getElementById("contenedor");
+var imgx=document.getElementById("x");
+var boton=document.getElementById('init');
+
+boton.addEventListener('click', start, false);
